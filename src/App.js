dbcaac8d719c708c70f1c513abb59c4ac7a0a89b
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-07 21:29:38 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
 import './App.css';
+import Button from './Components/Button';
+import ButtonBox from './Components/ButtonBox';
+import Screen from './Components/Screen';
 import Wrapper from './Components/Wrapper';
+
+const btnValues = [
+  ["C", "+-","%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0,".", "="],
+];
 
 function App() {
   return (
     <div className="App">
      <Wrapper>
-      <h1>App</h1>
+      <Screen/>
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn , i) =>(
+            <Button value = {btn} key={i}/>
+          ))
+        }
+        
+      </ButtonBox>
      </Wrapper>
     </div>
   );
