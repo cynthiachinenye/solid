fc09cb606c810fe5b8d73431b96149f643376c26
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-08 13:31:28 +0100

    new

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const getStyleName = cyndy =>{
+  const className ={
+    '=': 'equalto',
+    'x': 'options',
+    '-':'options',
+    '+': 'options',
+    '/': 'options',
+  }
+  return className[cyndy]
+}
 const Button = ({value}) => {
   return (
-<button > {value}</button >
+<button className={`${getStyleName(value)} button`}> {value}</button >
   )
 }
 
