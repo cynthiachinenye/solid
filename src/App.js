a57163fc3a04efde0a1f5baef3aa86436e995277
cynthiachinenye <cynthiachiagu@gmail.com>
2023-08-09 00:20:02 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import './App.css';
-import React,{useState} from './react'
-function App() {
+import React,{useState} from 'react'
 
+
+function App() {
+const [cal,setCal] = useState("")
   return (
     <div className="container">
        <div className="calculator">
         <form action="">
           <div className="screen">
-            <input type="text"/>
+            <input type="text" value={cal}/>
           </div>
           <div className="btns">
-            <input type="button" value="AC"/>
-            <input type="button" value="DE"/>
-            <input type="button" value="."/>
-            <input type="button" value="/"/>
+            <input type="button" value="AC" onClick={e => setCal("")}/>
+            <input type="button" value="DE" onClick={e => setCal(cal.slice(0,-1))}/>
+            <input type="button" value="." onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="/" onClick={e => setCal(cal + e.target.value)}/>
           </div>
           <div className="btns">
-            <input type="button" value="7"/>
-            <input type="button" value="8"/>
-            <input type="button" value="9"/>
-            <input type="button" value="x"/>
+            <input type="button" value="7" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="8"onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="9" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="*" onClick={e => setCal(cal + e.target.value)}/>
           </div>
           <div className="btns">
-            <input type="button" value="4"/>
-            <input type="button" value="5"/>
-            <input type="button" value="6"/>
-            <input type="button" value="+"/>
+            <input type="button" value="4" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="5" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="6" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="+" onClick={e => setCal(cal + e.target.value)}/>
           </div>
           <div className="btns">
-            <input type="button" value="1"/>
-            <input type="button" value="2"/>
-            <input type="button" value="3"/>
-            <input type="button" value="-"/>
+            <input type="button" value="1" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="2" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="3" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="-" onClick={e => setCal(cal + e.target.value)}/>
           </div>
           <div className="btns">
-            <input type="button" value="00"/>
-            <input type="button" value="0"/>
-            <input type="button" value="="/>
+            <input type="button" value="00" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="0" onClick={e => setCal(cal + e.target.value)}/>
+            <input type="button" value="=" className="equal" onClick={e => setCal(eval(cal))}/>
     
           </div>
         </form>
